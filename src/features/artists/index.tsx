161c2fd897417a007b0f1cec309c5ce298f9f5ac
bkuste2 <igorribeiro632@gmail.com>
2024-10-02 11:02:39 -0300

    :wrench: refactor: Refactor artists page

--- src/features/artists/index.tsx
+++ src/features/artists/index.tsx
@@ -1,13 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { List } from "@components/data-display/list.tsx";
-import { Avatar, Button, List as AntdList, Typography } from "antd";
+import { List as AntdList, Avatar, Button, Typography } from "antd";
 import { artistService } from "@/services/artist.service.ts";
 import React from "react";
 import { Artist } from "@/types/artist.model.ts";
 import { notification } from "@/services/default/notification.ts";
 
 export const Artists: React.FC = () => {
-	const { data, isFetched } = useQuery({
+	const { data, isFetched } = useQuery<Artist[]>({
 		queryKey: ["get-all-artists"],
 		queryFn: artistService.getAll,
 	});
@@ -15,40 +14,46 @@ export const Artists: React.FC = () => {
 	return (
 		<main className="flex h-[85vh] w-full items-center justify-center">
 			<div className="h-2/5 w-1/2">
-				{isFetched && (
-					<>
-						<Typography.Title level={1} className="self-start text-white">
-							Artists
-						</Typography.Title>
-						<div className="h-full overflow-y-auto pr-10">
-							<List<Artist>
-								data={data}
-								renderItem={(item, index) => (
-									<AntdList.Item
-										actions={[
-											<Button
-												onClick={() =>
-													notification("Just to improve template", "success")
+				{isFetched ? (
+					data && data.length > 0 ? (
+						<>
+							<Typography.Title level={1} className="self-start text-white">
+								Artists
+							</Typography.Title>
+							<div className="h-full overflow-y-auto pr-10">
+								<AntdList<Artist>
+									dataSource={data}
+									renderItem={(item, index) => (
+										<AntdList.Item
+											actions={[
+												<Button
+													onClick={() =>
+														notification("Just to improve template", "success")
+													}
+												>
+													Details
+												</Button>,
+											]}
+										>
+											<AntdList.Item.Meta
+												avatar={
+													<Avatar
+														src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+													/>
 												}
-											>
-												Details
-											</Button>,
-										]}
-									>
-										<AntdList.Item.Meta
-											avatar={
-												<Avatar
-													src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
-												/>
-											}
-											title={<a href="https://ant.design">{item.name}</a>}
-											description={item.twitter}
-										/>
-									</AntdList.Item>
-								)}
-							/>
-						</div>
-					</>
+												title={<a href="https://ant.design">{item.name}</a>}
+												description={item.twitter}
+											/>
+										</AntdList.Item>
+									)}
+								/>
+							</div>
+						</>
+					) : (
+						<Typography.Paragraph className="text-white">No artists found.</Typography.Paragraph>
+					)
+				) : (
+					<Typography.Paragraph className="text-white">Loading...</Typography.Paragraph>
 				)}
 			</div>
 		</main>

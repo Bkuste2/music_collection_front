eb94391a92eb351875de2d7e1c1e0b5fb35f2fbc
Bkuste2 <igorribeiro632@gmail.com>
2024-10-02 13:57:31 -0300

    :rocket: feat: Add update method in user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -12,6 +12,11 @@ class UserService {
 		const response = await api.post("/users", { ...data, role: "user" });
 		return response.data.data;
 	}
+
+	async update(id: string, data: Partial<RegisterProps>): Promise<User> {
+		const response = await api.patch(`/users/${id}`, data);
+		return response.data.data;
+	}
 }
 
 export const userService = new UserService();

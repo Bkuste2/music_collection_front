873c4c589218dde38f2c24ec478e9f5e1103520c
Bkuste2 <igorribeiro632@gmail.com>
2024-10-02 14:07:20 -0300

    :rocket: feat: Create a logic to change password

--- src/components/others/Header.tsx
+++ src/components/others/Header.tsx
@@ -1,22 +1,51 @@
-import { Text } from "../general/text.tsx";
-import { useAuth } from "@/contexts/auth-context.tsx";
-import { Button, Dropdown, MenuProps } from "antd";
-import { JsxRow } from "@components/layout/jsx-row.tsx";
 import { FEATURES } from "@/consts/features.ts";
+import { useAuth } from "@/contexts/auth-context.tsx";
+import { notification } from "@/services/default/notification.ts";
+import { userService } from "@/services/user.service.ts";
+import { JsxRow } from "@components/layout/jsx-row.tsx";
+import { useMutation } from "@tanstack/react-query";
+import { Button, Col, Dropdown, MenuProps, Modal } from "antd";
+import { useCallback, useState } from "react";
+import { FieldsBuilder } from "../builder/fields-builder.tsx";
+import { minLength, required } from "../data-entry/validators.ts";
+import { Text } from "../general/text.tsx";
+import { Form } from "../data-entry/form.tsx";
 
 export const Header = () => {
 	const { user, handleLogout } = useAuth();
+	const [open, setOpen] = useState<boolean>(false);
 
 	const items: MenuProps["items"] = [
 		{ key: "1", label: "My Account", disabled: true },
 		{ type: "divider" },
-		{ key: "2", label: "Change Password" },
+		{ key: "2", label: "Change Password", onClick: () => setOpen(true) },
 		{ key: "3", label: "Logout", onClick: handleLogout },
 	];
 
-	if (!user) {
-		return null;
-	}
+	const changePassword = useMutation({
+		mutationFn: ({ password }: { password: string }) => userService.update(user?.id as string, { password }),
+		onSuccess: () => {
+			notification("Password changed with successfull", "success");
+			setOpen(false);
+		},
+		onError: (error: any) => {
+			const errorData = error?.response?.data?.errors?.[0]?.id;
+			notification(errorData[1][0], "error");
+		},
+	});
+
+	const [form] = Form.useForm();
+
+	const handleValuesChange = useCallback(
+		(values: { password: string }) => {
+			form.setFieldsValue(values);
+		},
+		[form],
+	);
+
+	const handleFinish = useCallback(({ password }: { password: string }) => {
+		changePassword.mutate({ password });
+	}, []);
 
 	return (
 		<>
@@ -44,6 +73,34 @@ export const Header = () => {
 					</Dropdown>
 				</div>
 			</JsxRow>
+			<Modal
+				title="Basic Modal"
+				open={open}
+				onOk={form.submit}
+				onCancel={() => setOpen(false)}
+				okButtonProps={{ type: "default", htmlType: "submit" }}
+			>
+				<Form
+					form={form}
+					initialValues={{
+						password: "",
+					}}
+					onValuesChange={handleValuesChange}
+					onFinish={handleFinish}
+					layout="vertical"
+				>
+					<Col>
+						<FieldsBuilder
+							label="New Password"
+							name="password"
+							type="password"
+							size={24}
+							rules={[required(), minLength({ type: "string", length: 6 })]}
+							disabled={changePassword.isPending}
+						/>
+					</Col>
+				</Form>
+			</Modal>
 		</>
 	);
 };
